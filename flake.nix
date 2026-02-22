b9ef72f41a3fb1ed7e3b758c844e9dd1e110bd28
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-02-22 21:10:20 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix flake of Zhaoxiuya";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            direnv
+          ];
+        };
+      }
+    );
+}
